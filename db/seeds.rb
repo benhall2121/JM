8e174a5ed96038d4f6a2542965d90ec1109b39fa
Ben Hall <benhall2121@yahoo.com>
2011-05-12 09:51:16 -0700

    benhall2121@yahoo.com correct redirect after registering

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+user1 = User.create(:username => 'Ben', :email => 'ben@example.com', :password => '', :password_confirmation => '')
+user2 = User.create(:username => 'John', :email => 'john@example.com', :password => '', :password_confirmation => '')
+user3 = User.create(:username => 'Chris', :email => 'chris@example.com', :password => '', :password_confirmation => '')
+user4 = User.create(:username => 'Steve', :email => 'steve@example.com', :password => '', :password_confirmation => '')
